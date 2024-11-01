6b1d0fec2fccb74a17a8bd43fc6c43383f848459
João Rhoden <j.machado02@edu.pucrs.br>
2024-11-01 00:07:42 -0300

    sem pdf

--- src/app/dieta/page.js
+++ src/app/dieta/page.js
@@ -28,18 +28,18 @@ export default function QuizFinal() {
     }
   }
 
-  const handleExportPDF = () => {
-    if (typeof window === "undefined") return;
-    
-    const element = document.getElementById("diet-content"); // elemento a ser exportado
-    html2pdf().set({
-      margin: 1,
-      filename: "dieta2.pdf",
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
-    }).from(element).save();
-  };
+  // const handleExportPDF = () => {
+  //   if (typeof window === "undefined") return;
+
+  //   const element = document.getElementById("diet-content"); // elemento a ser exportado
+  //   html2pdf().set({
+  //     margin: 1,
+  //     filename: "dieta2.pdf",
+  //     image: { type: "jpeg", quality: 0.98 },
+  //     html2canvas: { scale: 2 },
+  //     jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+  //   }).from(element).save();
+  // };
 
   const formatResponseText = (text) => {
     const keywords = [
@@ -79,7 +79,7 @@ export default function QuizFinal() {
           <div className="flex flex-col items-center mb-4 bg-[#c7ccc3] p-5 rounded-xl w-10/12 md:w-4/6 xl:w-1/2">
             <h2 className="font-bold">🥗 Dieta:</h2>
             <pre id="diet-content"className="whitespace-pre-wrap text-left font-sans mt-3 pb-2">{formatResponseText(responseText.replace(/['"]+/g, ''))}</pre>
-            <button  className="mt-6 mb-2 px-4 py-2 bg-[#e44141cf] text-white rounded font-semibold hover:bg-[#acc6ba]" onClick={handleExportPDF}>
+            <button  className="mt-6 mb-2 px-4 py-2 bg-[#e44141cf] text-white rounded font-semibold hover:bg-[#acc6ba]">
               Exportar PDF
           </button>
           </div>
