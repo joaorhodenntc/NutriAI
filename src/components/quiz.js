a56647316f08316fa3bf367acbff7d0c2a9e9054
joao-rhoden <j.machado02@edu.pucrs.br>
2024-11-06 20:10:26 -0300

    add react-icons

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,11 +1,12 @@
 "use client"
 import Option from "./option";
+import { FaArrowLeft } from 'react-icons/fa'; 
 
 export default function Quiz({title, options, onAnswerSelect, onBackClick}) {
     return (
       <div className="flex flex-col items-start p-5 rounded-xl w-11/12 md:w-4/6 xl:w-1/3 shadow-md bg-gradient-to-br from-[#BCE0A1] to-[#8fbf6a]">
         <button onClick={onBackClick}>
-          Voltar
+          <FaArrowLeft />
         </button>
         <div className="flex flex-col items-center w-full">
           <h1 className="w-10/12 text-center text-lg font-semibold mb-2">{title}</h1>
