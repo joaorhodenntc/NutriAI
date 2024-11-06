8aeaf7a4a5e770aa23edfe085c537a7ec161adbd
joao-rhoden <j.machado02@edu.pucrs.br>
2024-11-06 20:05:42 -0300

    removi lib de icons

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,12 +1,11 @@
 "use client"
 import Option from "./option";
-import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
 export default function Quiz({title, options, onAnswerSelect, onBackClick}) {
     return (
       <div className="flex flex-col items-start p-5 rounded-xl w-11/12 md:w-4/6 xl:w-1/3 shadow-md bg-gradient-to-br from-[#BCE0A1] to-[#8fbf6a]">
         <button onClick={onBackClick}>
-          <ArrowLeftIcon className="h-5 w-5" />
+          Voltar
         </button>
         <div className="flex flex-col items-center w-full">
           <h1 className="w-10/12 text-center text-lg font-semibold mb-2">{title}</h1>
--- src/components/quizInput.js
+++ src/components/quizInput.js
@@ -1,6 +1,5 @@
 "use client"
 import { useState } from "react";
-import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
 export default function QuizInput({title, onChange, measure, onBackClick}) {
   const [value, setValue] = useState("");
@@ -23,7 +22,7 @@ export default function QuizInput({title, onChange, measure, onBackClick}) {
     return (
       <div className="flex flex-col items-start p-5 rounded-xl w-10/12 md:w-4/6 xl:w-1/3 bg-gradient-to-br from-[#BCE0A1] to-[#8fbf6a]">
         <button onClick={onBackClick}>
-          <ArrowLeftIcon className="h-5 w-5" />
+          Voltar
         </button>
         <div className="flex flex-col items-center">
           <h1 className="w-10/12 text-center text-lg font-semibold">{title}</h1>
