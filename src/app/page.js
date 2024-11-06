e9046484ad4f465bac8ecceefb55a677f370c865
João Rhoden <j.machado02@edu.pucrs.br>
2024-11-06 14:25:07 -0300

    alterando rota da tela inicial

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <button onClick={() => router.push("/quiz-1")} className="mb-4 px-4 py-2 bg-[#BCE0A1] rounded font-semibold">
+      <button onClick={() => router.push("/quiz")} className="mb-4 px-4 py-2 bg-[#BCE0A1] rounded font-semibold">
           Iniciar Teste 🚀
         </button>
     </div>
